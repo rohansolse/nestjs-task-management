fd7053f4ae34c645e95017a75575842ed59158fa
rohansolse <rohansolse@gmail.com>
2022-05-13 12:33:35 +0530

    task creation done

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,6 +9,11 @@ export class TasksController {
     @Get()
     getTasks(): Task[] {
         return this.tasksServices.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body('title') title: string, @Body('description') description: string): Task {
+        return this.tasksServices.createTask(title, description);
     }
 
 }
